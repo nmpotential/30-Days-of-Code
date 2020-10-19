c35b05d473732fe90ae9b9563a07d14f456ab78e
nmpotential <nathan.mokonz@gmail.com>
2020-10-19 02:27:56 +0100

    Complete Day 1

--- 01 - Data Types/Solution.go
+++ 01 - Data Types/Solution.go
@@ -13,16 +13,25 @@ func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
 
+	// Declare second integer, double, and String variables.
 	var i2 uint64
 	var d2 float64
 	var s2 string
 
+	// Read and save an integer, double, and String to your variables.
 	fmt.Scan(&i2)
 	fmt.Scan(&d2)
 	scanner.Scan()
 	s2 = scanner.Text()
 
+	// Print the sum of both integer variables on a new line.
 	fmt.Println(i + i2)
+
+	// Print the sum of the double variables on a new line.
 	fmt.Printf("%.1f\n", d+d2)
+
+	// Concatenate and print the String variables on a new line
+	// The 's' variable above should be printed first.
 	fmt.Printf("%s%s", s, s2)
+
 }
